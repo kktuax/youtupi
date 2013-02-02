61c111c5eda22499a610c4bea6171815345bc59c
Max Tuni <maxilandia@gmail.com>
2013-02-02 11:05:58 +0000

    initial version of server and client

---
+++ static/youtupi.js
@@ -0,0 +1,56 @@
+function loadPlayList(entries){
+	$("#playlist").empty();
+	for (var i = 0; i < entries.length; i++) {
+		var video = entries[i];
+		var itemval = $('<li><a href="#"><img src="'+ video.thumbnail + '" /><h3>' + video.title + '</h3><p>'+video.description + '</p></a></li>');
+		$("#playlist").append(itemval);
+	}
+	$("#playlist").listview("refresh");
+}
+function playerAction(paction){
+	var server = $("#server").val();
+	$.getJSON(
+		server + "/control/"+paction, loadPlayList
+	);
+}
+function loadVideo(video){
+	video.type = "youtube";
+	video.format = $("#quality").val();
+	var server = $("#server").val();
+	var url = server + "/playlist";
+	var data = $.toJSON(video);
+	$.post(url, data, loadPlayList, "json");
+}
+$(document).ready(function() {
+	$("#search-basic").bind("change", function(event, ui) {
+		$('#results').empty();
+		$("#results").listview("refresh");
+		if($("#search-basic").val() != ''){
+			var url = 'http://gdata.youtube.com/feeds/api/' + 
+				'videos?vq='+$("#search-basic").val()+'&max-results=15&v=2&alt=jsonc&orderby=relevance&sortorder=descending';
+			$.getJSON(url, function(response){
+				var entries = response.data.items || [];
+				for (var i = 0; i < entries.length; i++) {
+					var entry = entries[i];
+					var video = {}
+					video.id = entry.id;
+					video.description = entry.description;
+					video.title = entry.title;
+					video.thumbnail = entry.thumbnail.sqDefault;
+					var itemval = $('<li><a href="#"><img src="'+ video.thumbnail + '" /><h3>' + video.title + '</h3><p>'+video.description + '</p></a></li>');
+					itemval.bind('click', {video: video}, function(event) {
+						loadVideo(event.data.video);
+					});
+					$("#results").append(itemval);
+				}
+				$("#results").listview("refresh");
+			});
+		}
+	});
+	window.setInterval(function(){
+		var server = $("#server").val();
+		$.getJSON(
+			server + "/playlist", loadPlayList
+		);
+	}, 3000);
+});
---
+++ youtupi.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import web
+import os
+import signal
+import sys
+import subprocess
+import json
+from StringIO import StringIO
+
+urls = (
+	'/(.*)/', 'redirect',
+	'/playlist', 'playlist',
+	'/control/(.*)', 'control',
+	'/', 'index'
+)
+app = web.application(urls, globals())
+
+player = None
+videos = list()
+
+class Video:
+	def __init__(self, vid, data, url):
+		self.vid = vid
+		self.data = data
+		self.url = url
+		self.played = False
+
+class redirect:
+	def GET(self, path):
+		web.seeother('/' + path)
+
+class index:
+	def GET(self):
+		web.seeother('/static/youtupi.html')
+
+# curl -i -X POST -d '{"id": "M8GrahQw4zw", "type": "youtube", "format": "18"}' http://192.168.1.2:8080/playlist 
+class playlist:
+	def GET(self):
+		removeOldVideosFromPlaylist()
+		autoPlay()
+		playlistVideos = list()
+		for video in videos:
+			playlistVideos.append(video.data)
+		
+		return json.dumps(playlistVideos, indent=4)
+	
+	def POST(self):
+		data = json.load(StringIO(web.data()))
+		if not isVideoOnPlaylist(data['id']):
+			if(data['type'] == "youtube"):
+				url = getYoutubeUrl(data['id'], data['format'])
+				video = Video(data['id'], data, url)
+				videos.append(video)
+		
+		web.seeother('/playlist')
+		
+	def DELETE(self):
+		global videos
+		videos = list()
+		web.seeother('/playlist')
+
+def isVideoOnPlaylist(vid):
+	for video in videos:
+		if video.vid == vid:
+			return True
+	return False
+
+def removeOldVideosFromPlaylist():
+	global videos
+	viewedVideos = filter(lambda video:video.played==True, videos)
+	if isProcessRunning(player):
+		for vv in viewedVideos[:-1]:
+			videos.remove(vv)
+	else:
+		for vv in viewedVideos:
+			videos.remove(vv)
+
+def removeVideoFromPlaylist(vid):
+	global videos
+	videos = filter(lambda video:video.vid!=vid, videos)
+
+def playNextVideo():
+	global player
+	if isProcessRunning(player):
+		os.killpg(player.pid, signal.SIGTERM)
+	for video in videos:
+		if not video.played:
+			player = subprocess.Popen(['omxplayer', '-ohdmi', video.url], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn=os.setsid)
+			video.played = True
+			break
+
+def autoPlay():
+	if (not isProcessRunning(player)) and (len(videos) > 0):
+		playNextVideo()
+
+class control:
+	def GET(self, action):
+		if action == "play":
+			playNextVideo()
+		else:
+			if isProcessRunning(player):
+				if action == "stop":
+					player.stdin.write("q")
+					player = None
+				if action == "pause":
+					player.stdin.write("p")
+				if action == "volup":
+					player.stdin.write("+")
+				if action == "voldown":
+					player.stdin.write("-")
+				if action == "forward":
+					player.stdin.write("\x1B[C")
+				if action == "backward":
+					player.stdin.write("\x1B[D")
+		web.seeother('/playlist')
+
+def isProcessRunning(process):
+	if process:
+		if process.poll() == None:
+			return True
+	return False
+
+def getYoutubeUrl(video, vformat = None):
+	url = "http://www.youtube.com/watch?v=" + video
+	if not vformat: 
+		args = ['youtube-dl', '-g', url]
+	else:
+		args = ['youtube-dl', '-f', vformat, '-g', url]
+	
+	yt_dl = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	(url, err) = yt_dl.communicate()
+	if yt_dl.returncode != 0:
+		sys.stderr.write(err)
+		raise RuntimeError('Error getting URL.')
+		
+	return url.decode('UTF-8').strip()
+
+if __name__ == "__main__":
+	app.run()

e97205fb3519526d9ee407ec7b7f522f2dc7ed88
Max Tuni <maxilandia@gmail.com>
2013-04-26 19:54:41 +0200

    refactoring youtube query methods

--- static/youtupi.js
+++ static/youtupi.js
@@ -5,6 +5,11 @@ $(document).bind('pageinit', function () {
 });
 
 function fillVideoList(entries, listSelect, clickEvent){
+	if(typeof clickEvent === 'undefined') { 
+		clickEvent = function(event){
+			loadVideo(event.data.video);
+		}; 
+	}
 	$(listSelect).empty();
 	for (var i = 0; i < entries.length; i++) {
 		var video = entries[i];
@@ -46,51 +51,59 @@ function tabPlaylist(){
 	$(".link-playlist").first().trigger('click');
 }
 
+function getYoutubeQueryUrl(){
+	var url;
+	var query = $("#search-basic").val();
+	if(query != ''){
+		if(query.substring(0, 2) == "u:"){
+			query = query.substring(2, query.length);
+			url = 'https://gdata.youtube.com/feeds/api/users/'+query+'/uploads?v=2&alt=jsonc';
+		}else if(query.substring(0, 2) == "f:"){
+			query = query.substring(2, query.length);
+			url = 'http://gdata.youtube.com/feeds/api/users/'+query+'/favorites?v=2&alt=jsonc';
+		}else{
+			url = 'http://gdata.youtube.com/feeds/api/videos?vq='+query+'&max-results='+$('#slider').val()+'&v=2&alt=jsonc&orderby=relevance&sortorder=descending';
+		}
+	}
+	return url;
+}
+
+function getYoutubeResponseVideos(response){
+	var videos = [];
+	var entries = response.data.items || [];
+	for (var i = 0; i < entries.length; i++) {
+		var entry = entries[i];
+		if(typeof entry.video != 'undefined'){
+			entry = entry.video;
+		}
+		var video = {}
+		video.id = entry.id;
+		video.description = entry.description;
+		video.title = entry.title;
+		video.duration = entry.duration;
+		video.thumbnail = "images/video.png";
+		if(typeof entry.thumbnail != 'undefined'){
+			if(typeof entry.thumbnail.hqDefault != 'undefined'){
+				video.thumbnail = entry.thumbnail.hqDefault;
+			}else if(typeof entry.thumbnail.sqDefault != 'undefined'){
+				video.thumbnail = entry.thumbnail.sqDefault;
+			}
+		}
+		video.type = "youtube";
+		video.format = $("#quality").val();
+		videos.push(video);
+	}
+	return videos;
+}
+
 $(document).delegate("#youtube", "pageinit", function() {
 	$("#search-basic").bind("change", function(event, ui) {
 		$('#results').empty();
 		$("#results").listview("refresh");
-		var query = $("#search-basic").val();
-		if(query != ''){
-			var url;
-			if(query.substring(0, 2) == "u:"){
-				query = query.substring(2, query.length);
-				url = 'https://gdata.youtube.com/feeds/api/users/'+query+'/uploads?v=2&alt=jsonc';
-			}else if(query.substring(0, 2) == "f:"){
-				query = query.substring(2, query.length);
-				url = 'http://gdata.youtube.com/feeds/api/users/'+query+'/favorites?v=2&alt=jsonc';
-			}else{
-				url = 'http://gdata.youtube.com/feeds/api/videos?vq='+query+'&max-results='+$('#slider').val()+'&v=2&alt=jsonc&orderby=relevance&sortorder=descending';
-			}
+		var url = getYoutubeQueryUrl();
+		if(url !== undefined){
 			$.getJSON(url, function(response){
-				var entries = response.data.items || [];
-				for (var i = 0; i < entries.length; i++) {
-					var entry = entries[i];
-					if(typeof entry.video != 'undefined'){
-						entry = entry.video;
-					}
-					var video = {}
-					video.id = entry.id;
-					video.description = entry.description;
-					video.title = entry.title;
-					video.duration = entry.duration;
-					video.thumbnail = "images/video.png";
-					if(typeof entry.thumbnail != 'undefined'){
-						if(typeof entry.thumbnail.hqDefault != 'undefined'){
-							video.thumbnail = entry.thumbnail.hqDefault;
-						}else if(typeof entry.thumbnail.sqDefault != 'undefined'){
-							video.thumbnail = entry.thumbnail.sqDefault;
-						}
-					}
-					video.type = "youtube";
-					video.format = $("#quality").val();
-					var itemval = $('<li><a href="#"><img src="'+ video.thumbnail + '" /><h3>' + video.title + ' (' + Math.round(video.duration/60) + ':' + video.duration % 60 + ')' + '</h3><p>'+video.description + '</p></a></li>');
-					itemval.bind('click', {video: video}, function(event) {
-						loadVideo(event.data.video);
-					});
-					$("#results").append(itemval);
-				}
-				$("#results").listview("refresh");
+				fillVideoList(getYoutubeResponseVideos(response), "#results");
 			});
 		}
 	});
@@ -99,9 +112,7 @@ $(document).delegate("#youtube", "pageinit", function() {
 $(document).delegate("#files", "pageinit", function() {
 	$.getJSON(
 		server + "/local", function(entries){
-			fillVideoList(entries, "#filelist", function(event) {
-				loadVideo(event.data.video);
-			});
+			fillVideoList(entries, "#filelist");
 		}
 	);
 });

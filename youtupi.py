555e33e3d228130ec75053ce568ec63ff1264e23
Max Tuni <maxilandia@gmail.com>
2013-08-17 13:05:07 +0200

    .part extension while downloading videos

--- youtupi.py
+++ youtupi.py
@@ -75,10 +75,12 @@ def playNextVideo():
 				break
 
 def download(url, destination):
-	with open(destination, 'w') as f:
+	tdestination = destination + ".part"
+	with open(tdestination, 'w') as f:
 		try:
 			f.write(urllib2.urlopen(url).read())
 			f.close()
+			os.rename(tdestination, destination)
 		except urllib2.HTTPError:
 			raise RuntimeError('Error getting URL.')
 

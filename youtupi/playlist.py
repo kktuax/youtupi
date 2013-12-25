dbadedfa5c0e01ba3897e2485ad5e2c09e107d84
Max Tuni <maxilandia@gmail.com>
2013-12-25 13:17:44 +0100

    refactoring playlist

--- youtupi/playlist.py
+++ youtupi/playlist.py
@@ -33,40 +33,40 @@ def removeVideo(vid):
     
 def findVideoInPlaylist(vid):
     fvideos = filter(lambda video:video.vid==vid, videos)
-    if(len(fvideos)>0):
+    if fvideos:
         return fvideos[0]
     else:
         return None
 
+def stopPlayer():
+    global player
+    if isProcessRunning(player):
+        os.killpg(player.pid, signal.SIGTERM)
+        player = None
+
 def playNextVideo():
-    with lock:
-        global player
-        if isProcessRunning(player):
-            os.killpg(player.pid, signal.SIGTERM)
-            player = None
-            removeOldVideosFromPlaylist()
-        for video in videos:
-            if not video.played:
-                player = subprocess.Popen(['omxplayer', '-ohdmi', video.url], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn=os.setsid)
-                while not isProcessRunning(player):
-                    time.sleep(1)
-                video.played = True
-                break
+    viewedVideos = filter(lambda video:video.played==False, videos)
+    nextVideo = viewedVideos[:1]
+    if nextVideo:
+        playVideo(nextVideo[0].vid)
 
 def addVideo(video):
     videos.append(video)
 
 def playVideo(videoId):
-    svideo = None
-    for video in videos:
-        if video.vid == videoId:
-            svideo = video
-            break
-    if svideo:
-        videos.remove(svideo)
-        svideo.played = False
-        videos.insert(0, svideo)
-        playNextVideo()
+    with lock:
+        stopPlayer()
+        svideo = findVideoInPlaylist(videoId)
+        if svideo:
+            if svideo != videos[0]:
+                removeOldVideosFromPlaylist()
+                videos.remove(svideo)
+                videos.insert(0, svideo)
+            global player
+            player = subprocess.Popen(['omxplayer', '-ohdmi', svideo.url], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn=os.setsid)
+            while not isProcessRunning(player):
+                time.sleep(1)
+            svideo.played = True
 
 def autoPlay():
     threading.Timer(1, autoPlay).start()
@@ -82,10 +82,9 @@ def isProcessRunning(process):
 
 def controlPlayer(action):
     global player
-    global videos
     if action == "stop":
-        player.stdin.write("q")
-        player = None
+        stopPlayer()
+        global videos
         videos = list()
     if action == "pause":
         player.stdin.write("p")

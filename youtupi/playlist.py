2d321e87ecb176f92b02d27480dd579410990f6b
Max Tuni <maxilandia@gmail.com>
2014-03-02 15:31:15 +0100

    video start time and timeout

--- youtupi/playlist.py
+++ youtupi/playlist.py
@@ -1,4 +1,4 @@
-import os, subprocess, threading, time
+import os, subprocess, threading, time, re
 from youtupi.video import Video
 from youtupi.modules import local, youtube
 
@@ -51,13 +51,19 @@ def playNextVideo():
     viewedVideos = filter(lambda video:video.played==False, videos)
     nextVideo = viewedVideos[:1]
     if nextVideo:
-        playVideo(nextVideo[0].vid)
+        try:
+            playVideo(nextVideo[0].vid)
+        except RuntimeError:
+            print 'Error playing video'
+            removeVideo(nextVideo[0].vid)
 
 def addVideo(data):
     video = Video(data['id'], data)
     videos.append(video)
 
 lock = threading.RLock()
+
+TIMEOUT = 60
 
 def playVideo(videoId):
     with lock:
@@ -68,17 +74,38 @@ def playVideo(videoId):
                 videos.remove(svideo)
                 videos.insert(0, svideo)
                 removeOldVideosFromPlaylist()
-            if not svideo.url:
-                prepareVideo(svideo)
-            if svideo.url:
-                playerArgs = ["omxplayer", "-o", "hdmi"]
-                playerArgs.append(svideo.url)
-                print "Running player: " + " ".join(playerArgs)
-                global player
-                player = subprocess.Popen(playerArgs, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn=os.setsid)
-                while not isProcessRunning(player):
-                    time.sleep(1)
+            cont = 0
+            while not svideo.url:
+                time.sleep(1)
+                cont = cont + 1
+                if cont > TIMEOUT:
+                    raise RuntimeError('Error playing video: video not prepared')
+            playerArgs = ["omxplayer", "-o", "hdmi"]
+            playerArgs.append(svideo.url)
+            print "Running player: " + " ".join(playerArgs)
+            global player
+            player = subprocess.Popen(playerArgs, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn=os.setsid)
+            cont = 0
+            while not isProcessRunning(player):
+                time.sleep(1)
+                cont = cont + 1
+                if cont > TIMEOUT:
+                    raise RuntimeError('Error playing video')
+            svideo.data['started'] = int(time.time())
             svideo.played = True
+
+def getVideoDuration(svideo):
+    if svideo.url:
+        args = ["omxplayer", "-i", svideo.url]
+        omxplayeri = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        (stout, sterr) = omxplayeri.communicate()
+        if omxplayeri.returncode == 0:
+            response = sterr.decode('UTF-8').strip()
+            durationRegexp = re.compile(".+Duration: (\d+):(\d+):(\d+).+")
+            durationSearch = durationRegexp.search(response)
+            if durationSearch:
+                return 3600 * int(durationSearch.group(1)) + 60 * int(durationSearch.group(2)) + int(durationSearch.group(3))
+    return None
 
 videoUrlLock = threading.RLock()
 
@@ -89,7 +116,8 @@ def prepareVideo(video):
             if not url:
                 url = youtube.getUrl(video.data)
             video.url = url
-
+        if video.url and not video.duration:
+            video.duration = getVideoDuration(video)
 
 def autoPlay():
     removeOldVideosFromPlaylist()

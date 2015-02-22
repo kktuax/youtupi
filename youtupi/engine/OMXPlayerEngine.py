a53dd2bb0e97dfcebe4ebf31ec2209c1f7e912fb
Max Tuni <maxilandia@gmail.com>
2015-02-22 18:39:25 +0100

    catch dbus exceptions

--- youtupi/engine/OMXPlayerEngine.py
+++ youtupi/engine/OMXPlayerEngine.py
@@ -2,7 +2,7 @@ from youtupi.engine.PlaybackEngine import PlaybackEngine
 import os, subprocess, time, dbus
 
 TIMEOUT = 60
-DBUS_RETRY_LIMIT = 50
+DBUS_RETRY_LIMIT = 600
 SECONDS_FACTOR = 1000000
 
 '''
@@ -35,40 +35,46 @@ class OMXPlayerEngine(PlaybackEngine):
     
     def stop(self):
         if self.isPlaying():
-            self.omxController().Action(dbus.Int32("15"))
+            self.tryToSendAction(dbus.Int32("15"))
             cont = 0
             while self.isPlaying():
                 time.sleep(1)
                 cont = cont + 1
                 if cont > TIMEOUT:
-                    raise RuntimeError('Error stopping video')            
+                    print 'Unable to stop player'            
         self.player = None
     
     def togglePause(self):
-        if self.isPlaying():
-            self.omxController().Action(dbus.Int32("16"))
+        self.tryToSendAction(dbus.Int32("16"))
     
     def setPosition(self, seconds):
-        if self.isPlaying():
-            self.omxController().SetPosition(dbus.ObjectPath("/not/used"), dbus.Int64(seconds*SECONDS_FACTOR))
+        try:
+            if self.isPlaying():
+                self.omxController().SetPosition(dbus.ObjectPath("/not/used"), dbus.Int64(seconds*SECONDS_FACTOR))
+        except:
+            print 'Unable to set position'
     
     def getPosition(self):
-        if self.isPlaying():
-            return int(self.omxProps().Position())/SECONDS_FACTOR
+        try:
+            if self.isPlaying():
+                return int(self.omxProps().Position())/SECONDS_FACTOR
+        except:
+            print 'Unable to determine position'
         return None
     
     def getDuration(self):
-        if self.isPlaying():
-            return int(self.omxProps().Duration())/SECONDS_FACTOR
+        try:
+            if self.isPlaying():
+                return int(self.omxProps().Duration())/SECONDS_FACTOR
+        except:
+            print 'Unable to determine duration'
         return None
     
     def volumeUp(self):
-        if self.isPlaying():
-            self.omxController().Action(dbus.Int32("18"))
+        self.tryToSendAction(dbus.Int32("18"))
     
     def volumeDown(self):
-        if self.isPlaying():
-            self.omxController().Action(dbus.Int32("17"))
+        self.tryToSendAction(dbus.Int32("17"))
     
     def isPlaying(self):
         if self.player:
@@ -96,4 +102,9 @@ class OMXPlayerEngine(PlaybackEngine):
                 if retry >= DBUS_RETRY_LIMIT:
                     raise RuntimeError('Error loading player dbus interface')
     
-    
+    def tryToSendAction(self, action):
+        if self.isPlaying():
+            try:
+                self.omxController().Action(action)
+            except:
+                print 'Error connecting with player'

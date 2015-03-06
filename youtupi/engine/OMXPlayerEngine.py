fae571ee14840d3f61494619197ddd9c9c9043cc
Max Tuni <maxilandia@gmail.com>
2015-03-06 20:33:27 +0100

    new dbus addrress

--- youtupi/engine/OMXPlayerEngine.py
+++ youtupi/engine/OMXPlayerEngine.py
@@ -1,5 +1,5 @@
 from youtupi.engine.PlaybackEngine import PlaybackEngine
-import os, subprocess, dbus
+import os, subprocess, dbus, time
 
 SECONDS_FACTOR = 1000000
 DBUS_RETRY_LIMIT = 50
@@ -27,16 +27,18 @@ class OMXPlayerEngine(PlaybackEngine):
         playerArgs.append(video.url)
         print "Running player: " + " ".join(playerArgs)
         self.player = subprocess.Popen(playerArgs, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn=os.setsid)
+        time.sleep(0.5)
         retry=0
         while True:
             try:
-                with open('/tmp/omxplayerdbus', 'r+') as f:
+                with open('/tmp/omxplayerdbus.pi', 'r+') as f:
                     omxplayerdbus = f.read().strip()
                 bus = dbus.bus.BusConnection(omxplayerdbus)
                 dbobject = bus.get_object('org.mpris.MediaPlayer2.omxplayer','/org/mpris/MediaPlayer2', introspect=False)
                 self.props = dbus.Interface(dbobject,'org.freedesktop.DBus.Properties')
                 self.controller = dbus.Interface(dbobject,'org.mpris.MediaPlayer2.Player')
             except:
+                time.sleep(0.5)
                 retry+=1
                 if retry >= DBUS_RETRY_LIMIT:
                     raise RuntimeError('Error loading player dbus interface')

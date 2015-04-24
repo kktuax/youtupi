2f22c569ec48a169036c6307ee8a967a3900b25d
Max Tuni <maxilandia@gmail.com>
2015-04-24 21:35:40 +0200

    youtube api v3

--- static/youtupi.youtube.js
+++ static/youtupi.youtube.js
@@ -2,37 +2,31 @@ function getYoutubeQueryUrl(){
 	var url;
 	var query = $("#search-basic").val().trim();
 	if(query != ''){
-		if(query.substring(0, 2) == "u:"){
-			query = query.substring(2, query.length);
-			url = 'https://gdata.youtube.com/feeds/api/users/'+query+'/uploads?v=2&alt=jsonc';
-		}else if(query.substring(0, 2) == "f:"){
-			query = query.substring(2, query.length);
-			url = 'http://gdata.youtube.com/feeds/api/users/'+query+'/favorites?v=2&alt=jsonc';
-		}else{
-			url = 'http://gdata.youtube.com/feeds/api/videos?vq='+query+'&max-results='+$('#slider').val()+'&v=2&alt=jsonc&orderby=relevance&sortorder=descending';
-		}
+		url = 'https://www.googleapis.com/youtube/v3/search?order=relevance&part=snippet&q='+query+'&type=video&maxResults='+$('#slider').val()+'&key=AIzaSyAdAR3PofKiUSGFsfQ03FBEpVkVa1WA0J4';
 	}
 	return url;
 }
 
 function getYoutubeResponseVideos(response){
 	var videos = [];
-	var entries = response.data.items || [];
+	var entries = response.items || [];
 	for (var i = 0; i < entries.length; i++) {
 		var entry = entries[i];
 		if(typeof entry.video != 'undefined'){
 			entry = entry.video;
 		}
 		var video = {}
-		video.id = entry.id;
-		video.description = entry.description;
-		video.title = entry.title;
+		video.id = entry.id.videoId;
+		video.description = entry.snippet.description;
+		video.title = entry.snippet.title;
 		video.duration = entry.duration;
-		if(typeof entry.thumbnail != 'undefined'){
-			if(typeof entry.thumbnail.hqDefault != 'undefined'){
-				video.thumbnail = entry.thumbnail.hqDefault;
-			}else if(typeof entry.thumbnail.sqDefault != 'undefined'){
-				video.thumbnail = entry.thumbnail.sqDefault;
+		if(typeof entry.snippet.thumbnails != 'undefined'){
+			if(typeof entry.snippet.thumbnails.high != 'undefined'){
+				video.thumbnail = entry.snippet.thumbnails.high.url;
+			}else if(typeof entry.snippet.thumbnails.medium != 'undefined'){
+				video.thumbnail = entry.snippet.thumbnails.medium.url;
+			}else{
+				video.thumbnail = entry.snippet.thumbnails.default.url;
 			}
 		}
 		video.type = "youtube";

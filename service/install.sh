bbe582b888a3b089136ecc67b03814b0e577119d
Max Tuni <maxilandia@gmail.com>
2015-11-28 12:31:46 +0100

    updated pafy, requires now youtube-dl

--- service/install.sh
+++ service/install.sh
@@ -9,7 +9,7 @@ fi
 
 apt-get update
 apt-get install -y omxplayer python-pip python-magic python-dbus git
-pip install web.py beautifulsoup4
+pip install web.py beautifulsoup4 youtube_dl
 git clone git://github.com/kktuax/youtupi.git $YOUTUPI_HOME
 cd $YOUTUPI_HOME
 git submodule init

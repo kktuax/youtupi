42bb5c281a53cf9f9bedc85383b31188e770f2af
tunim <max@predictia.es>
2016-08-15 17:55:54 +0200

    vlc support for change audio track

--- youtupi/engine/VlcEngine.py
+++ youtupi/engine/VlcEngine.py
@@ -7,16 +7,16 @@ SECONDS_FACTOR = 1000
 @author: Max
 '''
 class VlcEngine(PlaybackEngine):
-    
+
     '''
     VLC playback engine
     '''
 
     player = None
-    
+
     def __init__(self):
         pass
-        
+
     def play(self, video):
         if not video.url:
             raise RuntimeError("Video URL not found")
@@ -27,20 +27,20 @@ class VlcEngine(PlaybackEngine):
         else:
             self.player.set_media(vlc.Instance().media_new(video.url))
         self.player.play()
-    
+
     def stop(self):
         if self.isPlaying():
-            self.player.stop()           
+            self.player.stop()
         self.player = None
-    
+
     def togglePause(self):
         if self.player:
             self.player.pause()
-    
+
     def setPosition(self, seconds):
         if self.isPlaying():
             self.player.set_time(SECONDS_FACTOR*seconds)
-    
+
     def getPosition(self):
         if self.player:
             posmillis = self.player.get_time()
@@ -48,24 +48,24 @@ class VlcEngine(PlaybackEngine):
                 return posmillis/SECONDS_FACTOR
             return 0
         return None
-    
+
     def getDuration(self):
         if self.player:
             durmillis = self.player.get_length()
             if durmillis > 0:
                 return durmillis/SECONDS_FACTOR
         return None
-    
+
     def volumeUp(self):
         if self.player:
             curvol = self.player.audio_get_volume()
             self.player.audio_set_volume(curvol + 5)
-    
+
     def volumeDown(self):
         if self.player:
             curvol = self.player.audio_get_volume()
             self.player.audio_set_volume(curvol - 5)
-    
+
     def seekBackSmall(self):
         if self.player:
             self.setPosition(max(self.getPosition() - 30, 0))
@@ -73,14 +73,24 @@ class VlcEngine(PlaybackEngine):
     def seekForwardSmall(self):
         if self.player:
             self.setPosition(min(self.getPosition() + 30, self.getDuration()))
-        
+
     def seekBackLarge(self):
         if self.player:
             self.setPosition(max(self.getPosition() - 600, 0))
-    
+
     def seekForwardLarge(self):
         if self.player:
             self.setPosition(min(self.getPosition() + 600, self.getDuration()))
+
+    def prevAudioTrack(self):
+        if self.player:
+            currentTrack = self.player.audio_get_track()
+            self.player.audio_set_track(currentTrack - 1)
+
+    def nextAudioTrack(self):
+        if self.player:
+            currentTrack = self.player.audio_get_track()
+            self.player.audio_set_track(currentTrack + 1)
 
     def isPlaying(self):
         if self.player:
@@ -89,7 +99,7 @@ class VlcEngine(PlaybackEngine):
             if pos and dur:
                 if pos < dur:
                     print 'Still playing (Position ' + str(pos) + ', duration ' + str(dur) + ")"
-                    return self.player.is_playing() or ((dur - pos) > 1) 
+                    return self.player.is_playing() or ((dur - pos) > 1)
                 else:
                     print 'Finished Playing'
             else:

a3e2b33b44451bbd6e4094be8b53074e2d61802a
Max Tuni <maxilandia@gmail.com>
2017-02-12 17:02:42 +0100

    Update install.sh

--- service/install.sh
+++ service/install.sh
@@ -10,7 +10,7 @@ fi
 apt-get update
 apt-get install -y omxplayer python-pip python-magic python-dbus git
 pip install web.py beautifulsoup4 youtube_dl betterprint
-git clone git://github.com/orithena/youtupi.git $YOUTUPI_HOME
+git clone git://github.com/kktuax/youtupi.git $YOUTUPI_HOME
 cd $YOUTUPI_HOME
 git submodule init
 git submodule update

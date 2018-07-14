36157246fca8bc1a9e11b956e0a1b86b04f82482
tunim <max@predictia.es>
2018-07-14 13:31:38 +0200

    support peerflix server presets

--- youtupi/modules/preset.py
+++ youtupi/modules/preset.py
@@ -4,15 +4,52 @@
 import web, json, urllib, hashlib, os
 from youtupi.util import config
 
+def presetFactory(data):
+	if isinstance(data, dict):
+		if data.get('type', '') == "peerflix-server":
+			return PeerflixServerPreset(data.get('url', ''))
+	return Preset(data)
+
+class Preset(object):
+
+	url = ''
+
+	def __init__(self, url):
+		self.url = url
+
+	def id(self):
+		m = hashlib.md5()
+		m.update(self.url)
+		return m.hexdigest()
+
+	def description(self):
+		return self.url
+
+	def title(self):
+		return os.path.basename(self.url)
+
+	def fetch(self):
+		response = urllib.urlopen(self.url)
+		content = json.loads(response.read())
+		return content
+
+class PeerflixServerPreset(Preset):
+	def fetch(self):
+		response = urllib.urlopen(self.url + '/torrents')
+		content = []
+		torrents = json.loads(response.read())
+		for torrent in torrents:
+			for torrentFile in torrent.get('files', []):
+				content.append({'type': 'url', 'id': self.url + torrentFile.get('link', ''), 'title': torrentFile.get('name', '')})
+		return content
+
 class list:
 
 	def GET(self):
 		presets = []
-		for presetUrl in config.conf.get('presets', []):
-			m = hashlib.md5()
-			m.update(presetUrl)
-			preset = {'id': m.hexdigest(), 'description': presetUrl, 'title': os.path.basename(presetUrl), 'type': 'search', 'operations' : []}
-			presets.append(preset)
+		for presetData in config.conf.get('presets', []):
+			preset = presetFactory(presetData)
+			presets.append({'id': preset.id(), 'description': preset.description(), 'title': preset.title(), 'type': 'search', 'operations' : []})
 		return json.dumps(presets, indent=4)
 
 class load:
@@ -20,12 +57,10 @@ class load:
 	def GET(self, presetId):
 		print "searching for preset " + presetId
 		data = []
-		for presetUrl in config.conf.get('presets', []):
-			m = hashlib.md5()
-			m.update(presetUrl)
-			if presetId == m.hexdigest():
-				response = urllib.urlopen(presetUrl)
-				data = json.loads(response.read())
+		for presetData in config.conf.get('presets', []):
+			preset = presetFactory(presetData)
+			if presetId == preset.id():
+				data = preset.fetch()
 				break
 		return json.dumps(data, indent=4)
 

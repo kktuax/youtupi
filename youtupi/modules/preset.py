217818a1b446150871d6d5ecaaac8b701bfdc1bf
kktuax <maxilandia@gmail.com>
2019-11-16 12:32:50 +0100

    podcast preset

--- youtupi/modules/preset.py
+++ youtupi/modules/preset.py
@@ -2,57 +2,79 @@
 # -*- coding: utf-8 -*-
 
 import web, json, urllib, hashlib, os
+import bs4 as bs
 from youtupi.util import config
 
 def presetFactory(data):
 	if isinstance(data, dict):
 		if data.get('type', '') == "peerflix-server":
-			return PeerflixServerPreset(data.get('url', ''))
+			return PeerflixServerPreset(data)
 		if data.get('type', '') == "TV-Online-TDT-Spain":
-			return TvOnlineTDTSpainPreset(data.get('url', ''))
-	return Preset(data)
+			return TvOnlineTDTSpainPreset(data)
+		if data.get('type', '') == "podcast":
+			return PodcastPreset(data)
+	return Preset({'url': data})
 
 class Preset(object):
 
-	url = ''
+	data = {}
 
-	def __init__(self, url):
-		self.url = url
+	def __init__(self, data):
+		self.data = data
+
+	def url(self):
+		return self.data.get('url', '')
 
 	def id(self):
 		m = hashlib.md5()
-		m.update(self.url)
+		m.update(self.url())
 		return m.hexdigest()
 
 	def description(self):
-		return self.url
+		if self.data.get('description', ''):
+			return self.data.get('description', '')
+		return self.url()
 
 	def title(self):
-		return os.path.basename(self.url)
+		if self.data.get('title', ''):
+			return self.data.get('title', '')
+		return os.path.basename(self.url())
 
 	def fetch(self):
-		response = urllib.urlopen(self.url)
+		response = urllib.urlopen(self.url())
 		content = json.loads(response.read())
 		return content
 
 class PeerflixServerPreset(Preset):
+	
 	def fetch(self):
-		response = urllib.urlopen(self.url + '/torrents')
+		response = urllib.urlopen(self.url() + '/torrents')
 		content = []
 		torrents = json.loads(response.read())
 		for torrent in torrents:
 			for torrentFile in torrent.get('files', []):
-				content.append({'type': 'url', 'id': self.url + torrentFile.get('link', ''), 'title': torrentFile.get('name', '')})
+				content.append({'type': 'url', 'id': self.url() + torrentFile.get('link', ''), 'title': torrentFile.get('name', '')})
 		return content
 
 class TvOnlineTDTSpainPreset(Preset):
+	
 	def fetch(self):
-		response = urllib.urlopen(self.url)
+		response = urllib.urlopen(self.url())
 		content = []
 		channels = json.loads(response.read())
 		for channel in channels:
 			if channel.get('enabled', False):
 				content.append({'type': 'url', 'id': channel.get('link_m3u8', ''), 'title': channel.get('name', '')})
+		return content
+
+class PodcastPreset(Preset):
+	
+	def fetch(self):
+		f = urllib.urlopen(self.url())
+		content = []
+		soup = bs.BeautifulSoup(f.read(), 'xml')
+		for item in soup.find_all('item'):
+			content.append({'type': 'url', 'id': item.enclosure['url'], 'title': item.title.text})
 		return content
 
 class list:

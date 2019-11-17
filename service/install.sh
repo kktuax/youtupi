05887f1b72af5c79a60069fdb980a776d2456724
Max Tuni <maxilandia@gmail.com>
2019-11-17 12:29:51 +0000

    updated install script

--- service/install.sh
+++ service/install.sh
@@ -7,8 +7,7 @@ if [ -d "$YOUTUPI_HOME" ]; then
 	rm -rf $YOUTUPI_HOME
 fi
 
-apt-get update
-apt-get install -y libpcre3 fonts-freefont-ttf omxplayer python-pip python-magic python-dbus python-lxml git 
+apt-get update && apt-get install -y libpcre3 fonts-freefont-ttf omxplayer python-pip python-magic python-dbus python-lxml git 
 pip install web.py beautifulsoup4 youtube_dl betterprint
 git clone git://github.com/kktuax/youtupi.git $YOUTUPI_HOME
 cd $YOUTUPI_HOME
@@ -16,5 +15,10 @@ git submodule init
 git submodule update
 cp youtupi.conf.example youtupi.conf
 chown -R $YOUTUPI_USER:$YOUTUPI_USER $YOUTUPI_HOME
-cp $YOUTUPI_HOME/service/youtupi /etc/init.d/
-update-rc.d youtupi defaults
+
+cp $YOUTUPI_HOME/service/youtupi.service /etc/systemd/system/
+cp $YOUTUPI_HOME/service/youtupi-update.service /etc/systemd/system/
+cp $YOUTUPI_HOME/service/youtupi-update.timer /etc/systemd/system/
+systemctl enable youtupi
+systemctl enable youtupi-update.timer
+systemctl start youtupi-update.timer
--- service/update.sh
+++ service/update.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+INSTALL_SCRIPT='https://raw.githubusercontent.com/kktuax/youtupi/master/service/install.sh'
 YOUTUPI_HOME=/home/pi/youtupi
 YOUTUPI_USER=pi
 
+systemctl stop youtupi
+
+apt-get update
+curl -s $INSTALL_SCRIPT | grep 'apt-get install -y' | awk '{split($0,a," -y "); print a[2]}' | apt-get install -y
+
+DEPS=$(curl -s $INSTALL_SCRIPT | grep 'pip install' | awk '{split($0,a," install "); print a[2]}')
+pip install -I $DEPS
 pip install --upgrade youtube_dl
+
 cd $YOUTUPI_HOME
 git pull
 git submodule update
 chown -R $YOUTUPI_USER:$YOUTUPI_USER $YOUTUPI_HOME
+
+systemctl start youtupi
